329464d161e3dbf6d74d804019d0b5cedcf61ede
Mahbubur Rahman <77193562+mahbubcsebd@users.noreply.github.com>
2024-09-22 13:37:17 +0600

    Update getSiteSettings.js

--- app/utils/getSiteSettings.js
+++ app/utils/getSiteSettings.js
@@ -1,7 +1,9 @@
 const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export async function getSiteSettings(lang = 'en') {
-    const res = await fetch(`${baseUrl}/${lang}/site-settings`, { revalidate: 0 });
+    const res = await fetch(`${baseUrl}/${lang}/site-settings`, {
+            cache: 'no-cache'
+        });
 
     if (!res.ok) {
         throw new Error('Failed to fetch Site Settings');
